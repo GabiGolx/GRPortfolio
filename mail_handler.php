3cfe96fd4134231248e91789d5ddb9f9c1cf726b
GabiGolx <gabo.ramirez0811@gmail.com>
2021-04-16 16:43:31 -0300

    including php

---
+++ mail_handler.php
@@ -0,0 +1,19 @@
+<?php 
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$message=$_POST['message'];
+
+		$to='gabrielramirezacosta@hotmail.com';
+		$subject=.$subject;
+		$message="Name: ".$name."\n"."Wrote the following: "."\n\n".$message;
+		$headers="From: ".$email
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Succesfully! Thank you"." ".$name.", I will contact you shortly!</h1>";
+		} else {
+			echo "Something went wrong!";
+		}
+	}
+ ?>
