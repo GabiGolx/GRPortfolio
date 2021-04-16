c960f3de6043f91898894800262f82e15d770afd
GabiGolx <gabo.ramirez0811@gmail.com>
2021-04-16 16:52:27 -0300

    fix bug

--- mail_handler.php
+++ mail_handler.php
@@ -6,9 +6,9 @@
 		$message=$_POST['message'];
 
 		$to='gabrielramirezacosta@hotmail.com';
-		$subject=.$subject;
+		$subject="Name: ".$subject;
 		$message="Name: ".$name."\n"."Wrote the following: "."\n\n".$message;
-		$headers="From: ".$email
+		$headers="From: ".$email;
 
 		if(mail($to, $subject, $message, $headers)){
 			echo "<h1>Sent Succesfully! Thank you"." ".$name.", I will contact you shortly!</h1>";
