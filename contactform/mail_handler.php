e0cd23b10c6e7b11becd1f8eb490edc47a9d0415
GabiGolx <gabo.ramirez0811@gmail.com>
2021-04-16 17:49:27 -0300

    a ver

--- contactform/mail_handler.php
+++ contactform/mail_handler.php
@@ -9,13 +9,15 @@
 		$subject="Name: ".$subject;
 		$message="Name: ".$name."\n"."Wrote the following: "."\n\n".$msg;
 		$headers="From: ".$email;
+	
+		mail($to, $subject, $message, $headers);
 
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
+		// if(mail($to, $subject, $message, $headers)){
+		// 	echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		// }
+		// else{
+		// 	echo "Something went wrong!";
+		// }
 	}
  ?> 
 
